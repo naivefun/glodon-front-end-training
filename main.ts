bcf98f073690033867b3e609d39d72c25d3ba9ae
PeiSong Xiong <peisong@outlook.sg>
2018-04-08 17:58:58 +0800

    添加 async/await 支持

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 import * as $ from 'jquery';
 
 $('h1').css('color', 'red');
+
+(async () => {
+  const response = await fetch('http://localhost:3003');
+  const text = await response.text();
+  console.log(text);
+})();
